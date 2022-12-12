76c99391165ae78a21cb418b50480ed74427b4e9
ivrolan <ivanlpzbrc01@gmail.com>
2022-12-12 11:16:28 +0100

    score changed. test saves a ga_instance
    Signed-off-by: ivrolan <ivanlpzbrc01@gmail.com>

--- 4-legged-robot-model_simulation/walking.py
+++ 4-legged-robot-model_simulation/walking.py
@@ -73,10 +73,10 @@ def robot_init( dt, body_pos, fixed = True , connect = p.GUI):
     return body_id, joint_ids
 
 
-
-def move_joints(body_id , angles):
+def original_move_joints(body_id , angles):
     maxForce = 2 #N/m
     #move movable joints
+    # print("Angles:", angles)
     for i in range(3):
         p.setJointMotorControl2(body_id, i, p.POSITION_CONTROL, 
                                 targetPosition = angles[0,i] , force = maxForce)
@@ -86,6 +86,20 @@ def move_joints(body_id , angles):
                                 targetPosition = angles[2,i] , force = maxForce) 
         p.setJointMotorControl2(body_id, 12 + i, p.POSITION_CONTROL, 
                                 targetPosition = angles[3,i] , force = maxForce)
+
+def move_joints(body_id , angles):
+    maxForce = 2 #N/m
+    #move movable joints
+    # print("Angles:", angles)
+    for i in range(3):
+        p.setJointMotorControl2(body_id, i, p.POSITION_CONTROL, 
+                                targetPosition = angles[0+i*4] , force = maxForce)
+        p.setJointMotorControl2(body_id, 4 + i, p.POSITION_CONTROL, 
+                                targetPosition = angles[1+i*4] , force = maxForce)
+        p.setJointMotorControl2(body_id, 8 + i, p.POSITION_CONTROL, 
+                                targetPosition = angles[2+i*4] , force = maxForce) 
+        p.setJointMotorControl2(body_id, 12 + i, p.POSITION_CONTROL, 
+                                targetPosition = angles[3+i*4] , force = maxForce)
         
 def get_paws_poses(body_id):
     angles = list()
@@ -94,12 +108,13 @@ def get_paws_poses(body_id):
         angles.append(p.getJointState(body_id, i+4)[0])
         angles.append(p.getJointState(body_id, i+8)[0])
         angles.append(p.getJointState(body_id, i+12)[0])
+    # print("GetPaws:", angles)
     return angles
         
         
 def robot_stepsim( body_id, body_pos, body_orn, body2feet ):
     angles, body2feet_ = robotKinematics.solve( body_orn, body_pos, body2feet)
-    move_joints(body_id, angles)
+    original_move_joints(body_id, angles)
 
 
 
@@ -138,11 +153,11 @@ def update_data():
 """
 
 #"""
-def sim(solution, sol_idx, control_model):
+def sim(solution, sol_idx, control_model, gui = False):
     dT = 0.002
     debugMode = "STATES"
     
-    bodyId, jointIds = robot_init( dt = dT, body_pos = [0,0,0], fixed = False , connect = p.DIRECT)
+    bodyId, jointIds = robot_init( dt = dT, body_pos = [0,0,0.13], fixed = False , connect = p.DIRECT if not gui else p.GUI)
     # printear joints para ver cual es la del cuerpo.
     meassure = systemStateEstimator(bodyId)
 
@@ -164,12 +179,14 @@ def sim(solution, sol_idx, control_model):
     N_par = 8
 
     # start record video
-    #p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "robot.mp4")
+    if gui:
+        p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "robot.mp4")
 
     robot_pos = list(p.getLinkState(bodyId, 0)[0]) #pos
     robot_orn = list(p.getLinkState(bodyId, 0)[1]) #orn
     paws_angs = get_paws_poses(bodyId)
     model_input = torch.tensor(robot_pos + robot_orn + paws_angs)
+    score = 0
     for k_ in range(0,N_steps):
         print(k_)
         #MAIN LOOP
@@ -180,9 +197,9 @@ def sim(solution, sol_idx, control_model):
                             solution=solution,
                             data=model_input)
 
-        #entradas de la red (18): robot_pos, robot_orn, paws_angs
-        #salidas de la red (12): paws_angs_out
-        print(model_out.tolist())
+        # entradas de la red (18): robot_pos, robot_orn, paws_angs
+        # salidas de la red (12): paws_angs_out
+        # print(model_out.tolist())
         move_joints(bodyId, model_out.tolist())
 
         robot_pos = list(p.getLinkState(bodyId, 0)[0]) #pos
@@ -190,12 +207,20 @@ def sim(solution, sol_idx, control_model):
         paws_angs = get_paws_poses(bodyId)
         model_input = torch.tensor(robot_pos + robot_orn + paws_angs)
         #update_data() # debug data
-        
         p.stepSimulation()
         #print(time.time() - lastTime)
+
+        vel_vec = list(p.getLinkState(bodyId, 0, computeLinkVelocity = True)[6])
+
+
+        den = (2 * abs(3 - robot_pos[0]) + abs(robot_pos[1]) - min(0, 0.3 - robot_pos[2]) - 3 * min(0, 0.2 - vel_vec[0])) # 0.115 original Z value when standing still
+        if den == 0:
+            score += 999999999
+        else:
+            score += 1 / den
+        
     robot_quit()
 
-    score = 0
     return score
 
 #"""
@@ -240,12 +265,12 @@ if __name__ == '__main__':
         
 
 
-        robot_pos = p.getLinkState(bodyId, 0)[0] #pos
+        robot_pos = list(p.getLinkState(bodyId, 0)[0]) #pos
         robot_orn = p.getLinkState(bodyId, 0)[1] #orn
         #print("pos,\torn,\tL,\tLrot,\tangle,\tT,\tsda")
         #print(pos, "\t", orn, "\t")
-        print(get_paws_poses(bodyId))
-        
+        #print(robot_pos[2])
+        print(list(p.getLinkState(bodyId, 0, computeLinkVelocity = True)[6]))
         bodytoFeet = trot.loop( L , angle , Lrot , T , offset , bodytoFeet0 , sda)
         robot_stepsim( bodyId, pos, orn, bodytoFeet )
         
