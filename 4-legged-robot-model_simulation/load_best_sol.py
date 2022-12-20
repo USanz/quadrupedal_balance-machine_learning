b8c0f090f22c0cc16cf722a28637d0851b1c4663
ivrolan <ivanlpzbrc01@gmail.com>
2022-12-20 19:30:31 +0100

    added parameter to load_best_sol and README_conf for croqueta
    Signed-off-by: ivrolan <ivanlpzbrc01@gmail.com>

--- 4-legged-robot-model_simulation/load_best_sol.py
+++ 4-legged-robot-model_simulation/load_best_sol.py
@@ -1,13 +1,16 @@
 import torch
 import pygad.torchga
 import pygad
+import sys
 
 from walking import sim
 
 from test_pygad_my_model import control_model, fitness_func, callback_generation
 
-
-saved_population = pygad.load(filename="./save_ga_instance.ga")
+if len(sys.argv) > 1:
+    saved_population = pygad.load(filename=sys.argv[1])
+else:
+    saved_population = pygad.load(filename="../species/jumping_boy.ga")
 
 # Returning the details of the best solution.
 solution, solution_fitness, solution_idx = saved_population.best_solution()
--- species/README_net_configurations.txt
+++ species/README_net_configurations.txt
@@ -1,4 +1,5 @@
 # Quadrupedal Balance - Machine Learning
 
+croqueta -> (19,20) (20,20) (20,12)
 jumping -> (19,20) (20,20) (20,12)
 to point -> (19,20) (20,20) (20,20) (20,12)
